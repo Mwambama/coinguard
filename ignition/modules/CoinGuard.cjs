1b14aac8baf4c2ae8f64606a5e0eb178d711a94a
Alisala Mwamba <allysingi16@icloud.com>
2026-01-11 17:32:00 -0600

    added some comments

--- ignition/modules/CoinGuard.cjs
+++ ignition/modules/CoinGuard.cjs
@@ -1,15 +1,9 @@
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
 module.exports = buildModule("CoinGuardModule", (m) => {
+           //deploys the contract
   const coinguard = m.contract("CoinGuardPayments");
-  return { coinguard };
-});
-ß
-module.exports = buildModule("CoinGuardModule", (m) => {
-  const coinguard = m.contract("CoinGuardPayments");
-
-  // AUTOMATION: Authorize the AI Agent immediately after deployment
-  // AGENT_PUBLIC_ADDRESS from MetaMask
+          // whitelist the wallet to call the settle function
   const agentAddress = "0x79Fa9D791601aA0EAB2da6db50DBFdcb5dCc0DAc"; 
   
   m.call(coinguard, "authorizeAgent", [agentAddress, true]);
